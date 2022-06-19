b732303d803b12e9ebeb575005d34ce1b40a6ac1
Hannes Braun <hannesbraun@mail.de>
2022-06-19 15:17:46 +0200

    Add version information

--- cmd/tcpmuxa/main.go
+++ cmd/tcpmuxa/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hannesbraun/tcpmuxa/config"
 	"github.com/hannesbraun/tcpmuxa/tcpmux"
 	"os"
 )
 
+const version = "1.0.0"
+
 func main() {
+	fmt.Println("tcpmuxa", version)
+
 	configPath := "tcpmuxa.conf"
 	if len(os.Args) > 1 {
 		configPath = os.Args[1]
